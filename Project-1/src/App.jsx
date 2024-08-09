f302efbc86e2717d09560395152f4dab470110cb
HamzaThepixelpro <ha444777ha@gmail.com>
2024-08-09 19:11:30 +0500

    1.1

---
+++ Project-1/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img src='/images/brand_logo.png' alt="logo"/>
+        </div>
+        <ul>
+          <li herf='#'>Menu</li>
+          <li herf='#'>Location</li>
+          <li herf='#'>About</li>
+          <li herf='#'>Contact</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  )
+}
+
+export default App
